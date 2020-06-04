0c0c97564b40e603679859abc443946fe73e2206
alpharr <tcmpvh8@gmail.com>
2020-06-04 09:13:31 -0400

    eeae

--- index.js
+++ index.js
@@ -19,27 +19,14 @@ bot.on("message", message=>{
             message.channel.send("➥ `BrokLands.minehut.gg`");
             console.log("Hey! Someone executed the /ip command!");
         break;
-    }
-})
-
-bot.on("message", message=>{
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]){
         case "help":
             message.channel.send("> ‣ **Help** \n > _ _ \n > **/ip** \n > **/help** \n > **/apply** \n > _ _");
         break;
-    }
-})
-
-bot.on("message", message=>{
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]){
         case "apply":
             message.channel.send("> ‣ **Apply** \n > You may apply in https://forms.gle/igAHXHMNR8jpKCyY8 !");
+        break;
+        case "fantasia":
+            message.channel.send("> **Fantasia's Discord:** \n > https://discord.gg/QMPkeq9");
         break;
     }
 })
@@ -53,23 +40,6 @@ bot.on('message', function(message){
 bot.on('message', function(message){
     if(message.content == "hello"){
         message.channel.send("Hey there!");
-    }
-})
-
-bot.on('message', function(message){
-    if(message.content == "hi"){
-        message.channel.send("Hey there!");
-    }
-})
-
-bot.on("message", message=>{
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]){
-        case "fantasia":
-            message.channel.send("> **Fantasia's Discord:** \n > https://discord.gg/QMPkeq9");
-        break;
     }
 })
 
